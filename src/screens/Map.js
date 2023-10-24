87794d45b981425af192ca32f316f45c941cd995
Gabriel Coronel <gabrielcoronel0303@gmail.com>
2023-10-24 13:57:33 -0600

    Funciona el mapa

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -1,23 +1,44 @@
-import { useRoute } from '@react-navigation/stack'
+import { useNavigation, useRoute } from '@react-navigation/native'
 import MapView, { Marker } from 'react-native-maps'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { StyleSheet } from 'react-native'
+import { Appbar } from 'react-native-paper'
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     width: "100%"
+  },
+  map: {
+    flex: 1
   }
 })
 
 export default () => {
   const route = useRoute()
+  const navigation = useNavigation()
 
   const { placeName, latitude, longitude } = route.params
 
   return (
     <SafeAreaView style={styles.container}>
-      <MapView>
+      <Appbar.Header
+        mode="center-aligned"
+        elevated
+        statusBarHeight={0}
+      >
+        <Appbar.Content
+          title={placeName}
+        />
+
+        <Appbar.BackAction
+          onPress={() => navigation.goBack()}
+        />
+      </Appbar.Header>
+
+      <MapView
+        style={styles.map}
+      >
         <Marker
           coordinate={{ latitude, longitude }}
           title={placeName}
